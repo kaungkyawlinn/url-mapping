348385703609f88916288227dd605bd6e3db8a08
KaungKyawLin <linkaungkyaw@gmail.com>
2023-06-21 15:55:51 +0630

    url-mapping

---
+++ src/main/java/com/example/urlmappingdemo/controller/WelcomeController.java
@@ -0,0 +1,27 @@
+package com.example.urlmappingdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+    @GetMapping("/greet")
+    public String greeting(Model model){
+        model.addAttribute("tagline","Hello");
+        return "welcome";
+    }
+
+    @GetMapping("/sayString")
+    public String sayString(Model model){
+        model.addAttribute("tagline"
+        ,"Say something");
+        return "welcome";
+    }
+    @GetMapping("/")
+    public String welcome(Model model){
+        model.addAttribute("tagline",
+                "Welcome Spring MVC");
+        return "welcome";
+    }
+}
